f696552d6e4cfee75c025525fa27da247f571673
Duygu Çabalar <162328086+DygCabalar@users.noreply.github.com>
2024-03-06 17:03:20 +0300

    Patika SQL Ödev 4

---
+++ Patika SQL Ödev 4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost FROM film;
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+SELECT COUNT (*) FROM film WHERE title LIKE 'T%' AND rating ='G'
+SELECT COUNT (*) FROM Country WHERE length (Country) =5;
+SELECT COUNT(*) FROM City WHERE city ILIKE '%R';
