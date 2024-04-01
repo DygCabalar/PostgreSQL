d2b6fade4b71798357f6f3f47b8521aa52653e08
Duygu Çabalar <162328086+DygCabalar@users.noreply.github.com>
2024-04-01 14:00:53 +0300

    Patika SQL Ödev 8.sql

---
+++ Patika SQL Ödev 8.sql
@@ -0,0 +1,84 @@
+1)CREATE TABLE employee ( 
+	id INTEGER , 
+	name VARCHAR(50) ,
+	birthday DATE ,
+	email VARCHAR(100)
+);
+
+2) insert into employee (id, name, birthday, email) values (1, 'Sheridan', '2023/09/18', 'snegro0@craigslist.org');
+insert into employee (id, name, birthday, email) values (2, 'Teddie', '2024/01/16', 'tboosey1@psu.edu');
+insert into employee (id, name, birthday, email) values (3, 'Vittoria', '2023/10/17', 'vsarver2@marketwatch.com');
+insert into employee (id, name, birthday, email) values (4, 'Nicki', '2023/10/13', 'nridsdole3@ocn.ne.jp');
+insert into employee (id, name, birthday, email) values (5, 'Brigitte', null, 'bmatuszyk4@deliciousdays.com');
+insert into employee (id, name, birthday, email) values (6, 'Franz', '2024/03/24', 'fpoter5@purevolume.com');
+insert into employee (id, name, birthday, email) values (7, 'Pietrek', '2023/10/27', 'pfoan6@google.nl');
+insert into employee (id, name, birthday, email) values (8, 'Nerissa', null, null);
+insert into employee (id, name, birthday, email) values (9, 'Beau', null, 'bbrinsden8@theguardian.com');
+insert into employee (id, name, birthday, email) values (10, 'Brandy', null, 'bmunby9@ft.com');
+insert into employee (id, name, birthday, email) values (11, 'Georgeanna', '2023/10/29', 'gaspinalla@jugem.jp');
+insert into employee (id, name, birthday, email) values (12, 'Kelila', '2024/03/08', 'kirvinb@sakura.ne.jp');
+insert into employee (id, name, birthday, email) values (13, 'Brander', '2024/01/15', 'btandeyc@cnet.com');
+insert into employee (id, name, birthday, email) values (14, 'Kincaid', '2023/06/08', 'kcapstaffd@dailymotion.com');
+insert into employee (id, name, birthday, email) values (15, 'Merissa', '2023/05/21', 'mladewige@bravesites.com');
+insert into employee (id, name, birthday, email) values (16, 'Edd', '2023/05/17', 'ehumbatchf@jiathis.com');
+insert into employee (id, name, birthday, email) values (17, 'Savina', '2023/04/23', null);
+insert into employee (id, name, birthday, email) values (18, 'Pamelina', '2023/12/10', 'pnazarethh@prweb.com');
+insert into employee (id, name, birthday, email) values (19, 'Raimund', null, 'rhubyi@washingtonpost.com');
+insert into employee (id, name, birthday, email) values (20, 'Beau', '2023/07/29', 'bgowerj@list-manage.com');
+insert into employee (id, name, birthday, email) values (21, 'Valdemar', '2023/10/09', null);
+insert into employee (id, name, birthday, email) values (22, 'Miquela', '2023/09/06', 'molensbyl@cafepress.com');
+insert into employee (id, name, birthday, email) values (23, 'Bernie', '2023/06/18', 'bcrossfieldm@howstuffworks.com');
+insert into employee (id, name, birthday, email) values (24, 'Shermy', '2023/06/17', 'sharmann@va.gov');
+insert into employee (id, name, birthday, email) values (25, 'Ferne', null, null);
+insert into employee (id, name, birthday, email) values (26, 'Tiffani', '2023/10/27', 'tferenczp@amazon.co.jp');
+insert into employee (id, name, birthday, email) values (27, 'Ignatius', null, 'iloakesq@fotki.com');
+insert into employee (id, name, birthday, email) values (28, 'Alina', '2023/09/23', 'adowreyr@goo.gl');
+insert into employee (id, name, birthday, email) values (29, 'Meagan', null, null);
+insert into employee (id, name, birthday, email) values (30, 'Tricia', '2023/04/27', null);
+insert into employee (id, name, birthday, email) values (31, 'Camel', '2023/12/06', 'cthickettu@geocities.com');
+insert into employee (id, name, birthday, email) values (32, 'Danyelle', '2023/10/12', 'dmacgaughiev@samsung.com');
+insert into employee (id, name, birthday, email) values (33, 'Suzie', '2023/07/03', 'skupkerw@guardian.co.uk');
+insert into employee (id, name, birthday, email) values (34, 'Pepi', null, 'psealeafx@vinaora.com');
+insert into employee (id, name, birthday, email) values (35, 'Meriel', '2023/04/23', 'materidgey@china.com.cn');
+insert into employee (id, name, birthday, email) values (36, 'Hammad', '2023/04/21', 'hcarlosz@google.nl');
+insert into employee (id, name, birthday, email) values (37, 'Berti', '2023/07/28', 'bambridge10@theguardian.com');
+insert into employee (id, name, birthday, email) values (38, 'Slade', '2023/12/27', null);
+insert into employee (id, name, birthday, email) values (39, 'Eldin', '2023/10/14', 'enye12@upenn.edu');
+insert into employee (id, name, birthday, email) values (40, 'Bronny', null, 'bstate13@prnewswire.com');
+insert into employee (id, name, birthday, email) values (41, 'Anjela', null, null);
+insert into employee (id, name, birthday, email) values (42, 'Julie', '2023/05/09', 'jbratchell15@bluehost.com');
+insert into employee (id, name, birthday, email) values (43, 'Charissa', '2023/11/18', 'colagen16@ucoz.com');
+insert into employee (id, name, birthday, email) values (44, 'Rainer', '2023/10/17', 'rreeveley17@ustream.tv');
+insert into employee (id, name, birthday, email) values (45, 'Augustina', null, 'amarch18@nhs.uk');
+insert into employee (id, name, birthday, email) values (46, 'Celina', '2023/06/04', 'cmarchetti19@amazon.de');
+insert into employee (id, name, birthday, email) values (47, 'Aurea', null, 'aclymo1a@biglobe.ne.jp');
+insert into employee (id, name, birthday, email) values (48, 'Hazel', '2023/09/07', 'hdonan1b@qq.com');
+insert into employee (id, name, birthday, email) values (49, 'Yehudit', null, 'yrobins1c@ehow.com');
+insert into employee (id, name, birthday, email) values (50, 'Costa', '2023/10/30', 'csea1d@yellowbook.com');
+
+3)UPDATE employee SET name = 'Duygu', birthday = '1994-04-26', email = 'atarduyguu@gmail.com' Where id = 3;
+UPDATE employee SET name = 'Ali', birthday = '1994-04-12' Where name = 'Franz' Returning *;
+UPDATE employee SET name = 'Ayşe', birthday = '1994-02-12' Where name Like 'A%a' Returning *;
+UPDATE employee SET name = 'Mustafa', birthday = '1989-01-15' Where name = 'Edd'  Returning *;
+UPDATE employee SET name = 'Aylin', email = 'xyz@gmail.com' Where name = 'Pamelina'  Returning *;
+
+
+4)Delete From employee 
+Where id = 2
+Returning *;
+
+Delete From employee 
+Where id Between 35 and 40
+Returning *;
+
+Delete From employee 
+Where name Like 'Ne%'
+Returning *;
+
+Delete From employee 
+Where id >45
+Returning *;
+
+Delete From employee 
+Where name ILIKE '%a%a%'
+Returning *;
